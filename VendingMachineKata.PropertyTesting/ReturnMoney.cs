402154ae0428c898544ddebfe27a84db5813cde6
Jakub Ciechowski <jciechowski@gmail.com>
2020-02-22 20:32:57 +0100

    return money test

--- VendingMachineKata.PropertyTesting/ReturnMoney.cs
+++ VendingMachineKata.PropertyTesting/ReturnMoney.cs
@@ -4,5 +4,33 @@ using FsCheck.Xunit;
 
 namespace VendingMachineKata.PropertyTesting
 {
-   
+    internal static class CoinsGenerator
+    {
+        public static decimal[] AllowedCoins = { 1m, 0.50m, 0.25m };
+
+        public static Arbitrary<string> GenerateCoins()
+        {
+            return Gen.Elements(AllowedCoins)
+                .ListOf()
+                .Select(coins => string.Join(", ", coins))
+                .ToArbitrary();
+        }
+    }
+
+    public class ReturnMoneyTests
+    {
+        [Property(Arbitrary = new[] { typeof(CoinsGenerator) })]
+        public Property ReturnsInsertedMoney(string coins)
+        {
+            var vendingMachine = new VendingMachine();
+            Func<bool> returnMoneyProperty = () =>
+            {
+                vendingMachine.InsertMoney(coins);
+
+                return vendingMachine.Return() == coins;
+            };
+
+            return returnMoneyProperty.ToProperty();
+        }
+    }
 }
