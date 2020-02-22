39034ffb6a5a9e2690af06277584d6a81a115db5
Jakub Ciechowski <jciechowski@gmail.com>
2020-02-22 07:22:02 +0100

    tests for buying cola

--- VendingMachineKata.Tests/BuyingSpecs.cs
+++ VendingMachineKata.Tests/BuyingSpecs.cs
@@ -3,5 +3,37 @@ using Xunit;
 
 namespace VendingMachineKata.Tests
 {
-    
+    public class BuyingProductTests
+    {
+        [Theory]
+        [InlineData("1", "Cola")]
+        [InlineData("0.5, 0.5", "Cola")]
+        [InlineData("0.25, 0.25, 0.25, 0.25", "Cola")]
+        public void BuyColaWhenInsertedEnoughMoney(string insertedCoins, string expectedProduct)
+        {
+            var subject = new VendingMachine();
+
+            subject.InsertMoney(insertedCoins);
+
+            var result = subject.GetCola();
+
+            result.Should().Contain(expectedProduct);
+        }
+
+        [Fact]
+        public void BuyMultipleProductsWhenInsertedEnoughMoney()
+        {
+            var subject = new VendingMachine();
+
+            subject.InsertMoney("1");
+
+            subject.GetCola();
+
+            subject.InsertMoney("1");
+
+            var result = subject.GetCola();
+
+            result.Should().Contain("Cola");
+        }
+    }
 }
