91867018080f3e7476b2b9367848ba1ef2c67336
Jakub Ciechowski <jciechowski@gmail.com>
2020-02-22 20:33:16 +0100

    return product with change test

--- VendingMachineKata.PropertyTesting/GetProductWithChange.cs
+++ VendingMachineKata.PropertyTesting/GetProductWithChange.cs
@@ -5,5 +5,37 @@ using FsCheck.Xunit;
 
 namespace VendingMachineKata.PropertyTesting
 {
+    public static class CoinsWithChangeGenerator
+    {
+        private static readonly decimal[] AllowedCoins = { 1m, 0.50m, 0.25m };
 
+        public static Arbitrary<(string, string)> GenerateCoinsWithChange()
+        {
+            return Gen.Elements(AllowedCoins)
+                .ListOf()
+                .Where(x => x.Sum() > 1)
+                .Select(coins => (string.Join(", ", coins), (coins.Sum() - 1m).ToString()))
+                .ToArbitrary();
+        }
+    }
+
+    public class BuyingProductWithChange
+    {
+        [Property(Arbitrary = new []{typeof(CoinsWithChangeGenerator)}, MaxTest = 1000)]
+        public Property GetsProductWithCorrectChange((string coins, string change) coinsWithChange)
+        {
+            var vendingMachine = new VendingMachine();
+            var (coins, change) = coinsWithChange;
+            Func<bool> productWithChangeProperty = () =>
+            {
+                vendingMachine.InsertMoney(coins);
+
+                var productWithChange = vendingMachine.GetCola();
+
+                return productWithChange.Contains("Cola") && productWithChange.Contains(change);
+            };
+
+            return productWithChangeProperty.ToProperty();
+        }
+    }
 }
