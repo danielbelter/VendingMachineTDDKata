88778e99aa67c7a497feb81e5c1c2263064b1e00
Jakub Ciechowski <jciechowski@gmail.com>
2020-02-22 07:06:22 +0100

    return money tests

--- VendingMachineKata.Tests/ReturnSpecs.cs
+++ VendingMachineKata.Tests/ReturnSpecs.cs
@@ -3,5 +3,22 @@ using Xunit;
 
 namespace VendingMachineKata.Tests
 {
-   
+    public class ReturnSpecs
+    {
+        [Theory]
+        [InlineData("1, 0.5, 0.5", "1, 0.5, 0.5")]
+        [InlineData("", "")]
+        [InlineData("1", "1")]
+        [InlineData("0.25, 0.25", "0.25, 0.25")]
+        public void ReturnsAllInsertedMoney(string insertedMoney, string expectedReturn)
+        {
+            var subject = new VendingMachine();
+
+            subject.InsertMoney(insertedMoney);
+
+            var result = subject.Return();
+
+            result.Should().Be(expectedReturn);
+        }
+    }
 }
